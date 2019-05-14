191f1eb41acb24dc2342ac43b813b38ffeeec7e5
ZraVarZ <zravarz@gmail.com>
2019-05-14 14:01:04 +0200

    Fixed a small bug in the database

--- src/main/java/controller/FoMenuController.java
+++ src/main/java/controller/FoMenuController.java
@@ -5,27 +5,23 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import AB.Eredmeny;
-import AB.XMLOlvaso;
+import ab.Eredmeny;
+import ab.XMLOlvaso;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class FoMenuController implements Initializable {
-
-    @FXML
-    private AnchorPane mindennekAzAlapja;
 
     @FXML
     private Pane alapMenuPane;
@@ -37,22 +33,7 @@ public class FoMenuController implements Initializable {
     private Pane eredmenyekPane;
 
     @FXML
-    private Button kezdesButton;
-
-    @FXML
-    private Button eredmenyekButton;
-
-    @FXML
-    private Button kilepesButton;
-
-    @FXML
-    private Button adjMegNevetButton;
-
-    @FXML
     private TextField adjMegNevetTextField;
-
-    @FXML
-    private Button eredmenyekExitButton;
 
     @FXML
     private TableView tablazat;
@@ -80,10 +61,13 @@ public class FoMenuController implements Initializable {
     }
 
     public void eredmenyekB(ActionEvent event){
-        tablazatFeltoltes();
-        alapMenuPane.setOpacity(0.5);
-        alapMenuPane.setDisable(true);
-        eredmenyekPane.setVisible(true);
+        File bevitel = new File("Pontok.xml");
+        if (bevitel.exists() && !bevitel.isDirectory()){
+            tablazatFeltoltes();
+            alapMenuPane.setOpacity(0.5);
+            alapMenuPane.setDisable(true);
+            eredmenyekPane.setVisible(true);
+        }
     }
 
     public void handleEredmenyekExitButton(ActionEvent event){
